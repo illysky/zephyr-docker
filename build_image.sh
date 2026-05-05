ad905fcc36cb3774b9c21087873226273363137c
Dominic Moffat <dom@illysky.com>
2026-05-05 12:56:11 +0000

    chore: update default version to v3.3.0, tag GHCR image in local build

--- build_image.sh
+++ build_image.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Set version once
-NCS_VERSION=${1:-v3.1.1}  # Default to v3.1.1, or use first argument
+NCS_VERSION=${1:-v3.3.0}  # Default to v3.3.0, or use first argument
+GHCR_IMAGE="ghcr.io/illysky/nrfconnect-sdk:${NCS_VERSION}"
 
 docker build \
   -t nrfconnect-sdk:${NCS_VERSION} \
+  -t ${GHCR_IMAGE} \
   --build-arg USER_UID=$(id -u) \
   --build-arg sdk_nrf_revision=${NCS_VERSION} \
   --network=host \
   .
 
 echo "Built image: nrfconnect-sdk:${NCS_VERSION}"
+echo "Tagged as:   ${GHCR_IMAGE}"
